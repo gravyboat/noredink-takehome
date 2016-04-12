8f784af144709ae7aef02600880f9873ddee6487
Forrest Alvarez <forrest.alvarez@gmail.com>
2016-04-11 21:43:17 -0700

    Restructure a bit and prep csvs as dicts.

--- noredink-takehome/nri.py
+++ noredink-takehome/nri.py
@@ -1,29 +1,28 @@
 import csv
 
+def user_question_prompt():
+    # prompt user for how many questions they want in the quiz,
+    # accept anything more than 0
+    question_count = raw_input("Please input the number of questions you would like to answer: ")
+    if question_count == 0:
+        print("0 is not sadly not accepted!")
+        user_question_prompt()
+    elif question_count == 1:
+        print("Thanks! I'm putting together {0} question now.".format(question_count))
+    else:
+        print("Thanks! I'm putting together {0} questions now.".format(question_count))
+    return(question_count)
 
-def read_in_csvs():
+    # Display question IDs based on strand values
+
+def pull_csv_data(num_questions):
 
     with open('questions.csv', 'rb') as csv_questions:
-        questions_reader = csv.reader(csv_questions, delimiter=',')
-        for row in questions_reader:
-            print(','.join(row))
+        questions_reader = csv.DictReader(csv_questions)
 
 
     with open('usage.csv', 'rb') as csv_usage:
         usage_reader = csv.DictReader(csv_usage)
-        for row in usage_reader:
-            print(row['student_id'])
-
-def user_question_prompt():
-    # prompt user for how many questions they want in the quiz,
-    # accept anything more than 0
-    question_count = raw_input("Please input the number of questions: ")
-    if question_count == 0:
-        print("0 is not accepted!")
-        user_question_prompt()
-    else:
-        print("Thanks! I'm putting {0} questions together now.".format(question_count))
-
-    # Display question IDs
-
-user_question_prompt()
+    
+num_questions = user_question_prompt()
+pull_csv_data(num_questions)
